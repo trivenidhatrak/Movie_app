b068929e03007147ee66d00070978c7a6a951a2c
trivenidhatrak <33419719+trivenidhatrak@users.noreply.github.com>
2017-11-24 16:12:55 +0530

    Update Movie_list.js

--- src/components/Movie_list.js
+++ src/components/Movie_list.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import axios from "axios";
-import cookie from 'react-cookies'
 import MyButton from './MyButton.js';
 class Movie_list extends Component {
  constructor() {
